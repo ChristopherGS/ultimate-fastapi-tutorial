6600a1ecb12fca92fb39c5e28f3bc3978d3a6eb6
ChristopherGS <christopher.samiullah@gmail.com>
2021-06-23 09:27:41 +0100

    finish part1 endpoint

--- part-1-hello-world/app/main.py
+++ part-1-hello-world/app/main.py
@@ -4,23 +4,28 @@ from typing import Optional
 
 
 RECIPES = [
-    {'label': "Chicken Vesuvio",
-     "source": "Serious Eats",
-     "url":"http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html"},
-    {"label": "Chicken Paprikash",
-     "source":"No Recipes",
-     "url":"http://norecipes.com/recipe/chicken-paprikash/"},
-    {"label": "Cauliflower and Tofu Curry Recipe",
-     "source": "Serious Eats",
-     "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html"}
+    {
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
 ]
 
 
-app = FastAPI(
-    title="Recipe API", openapi_url="/openapi.json"
-)
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+
 
 @api_router.get("/", status_code=200)
 def root() -> dict:
@@ -31,14 +36,19 @@ def root() -> dict:
 
 
 @api_router.get("/search/", status_code=200)
-def search_recipes(keyword: Optional[str] = None) -> dict:
+def search_recipes(
+    keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
     """
     Search for recipes based on label keyword
     """
+    if not keyword:
+        # we use Python list slicing to limit results
+        # based on the max_results query parameter
+        return {"results": RECIPES[:max_results]}
 
-    results = filter(
-        lambda recipe:  keyword.lower() in recipe['label'].lower(), RECIPES)
-    return {"results": list(results)}
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
 
 
 app.include_router(api_router)
@@ -47,4 +57,5 @@ app.include_router(api_router)
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
