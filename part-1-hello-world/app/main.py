47e93fdad6e170b0bd08fd4960b50b0033f83455
ChristopherGS <christopher.samiullah@gmail.com>
2021-06-24 09:35:51 +0100

    simplify part 1

--- part-1-hello-world/app/main.py
+++ part-1-hello-world/app/main.py
@@ -1,26 +1,5 @@
 from fastapi import FastAPI, APIRouter
 
-from typing import Optional
-
-
-RECIPES = [
-    {
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
-    },
-    {
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
-    },
-    {
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
-    },
-]
-
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
@@ -33,22 +12,6 @@ def root() -> dict:
     Root GET
     """
     return {"msg": "Hello, World!"}
-
-
-@api_router.get("/search/", status_code=200)
-def search_recipes(
-    keyword: Optional[str] = None, max_results: Optional[int] = 10
-) -> dict:
-    """
-    Search for recipes based on label keyword
-    """
-    if not keyword:
-        # we use Python list slicing to limit results
-        # based on the max_results query parameter
-        return {"results": RECIPES[:max_results]}
-
-    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
-    return {"results": list(results)[:max_results]}
 
 
 app.include_router(api_router)
