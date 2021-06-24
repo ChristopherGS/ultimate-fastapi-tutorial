3be3924bfa0db58e5a5a18b3579ad8815650590f
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2021-06-24 09:39:31 +0100

    Part 1 (#1)

---
+++ part-1-hello-world/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    """
+    return {"msg": "Hello, World!"}
+
+
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
