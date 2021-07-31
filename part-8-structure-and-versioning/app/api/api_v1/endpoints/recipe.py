650e646d70ed778fcfe00742b7ae6446313c7af3
ChristopherGS <christopher.samiullah@gmail.com>
2021-07-31 19:53:56 +0100

    add part 8

---
+++ part-8-structure-and-versioning/app/api/api_v1/endpoints/recipe.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from typing import Any, Optional
+
+from app import crud
+from app.api import deps
+from app.schemas.recipe import Recipe, RecipeCreate, RecipeSearchResults
+
+router = APIRouter()
+
+
+@router.get("/{recipe_id}", status_code=200, response_model=Recipe)
+def fetch_recipe(
+    *,
+    recipe_id: int,
+    db: Session = Depends(deps.get_db),
+) -> Any:
+    """
+    Fetch a single recipe by ID
+    """
+    result = crud.recipe.get(db=db, id=recipe_id)
+    if not result:
+        # the exception is raised, not returned - you will get a validation
+        # error otherwise.
+        raise HTTPException(
+            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
+        )
+
+    return result
+
+
+@router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+def search_recipes(
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
+    max_results: Optional[int] = 10,
+    db: Session = Depends(deps.get_db),
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    recipes = crud.recipe.get_multi(db=db, limit=max_results)
+    if not keyword:
+        return {"results": recipes}
+
+    results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), recipes)
+    return {"results": list(results)[:max_results]}
+
+
+@router.post("/", status_code=201, response_model=Recipe)
+def create_recipe(
+    *, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)
+) -> dict:
+    """
+    Create a new recipe (in memory only)
+    """
+    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
+
+    return recipe
---
+++ part-8-structure-and-versioning/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer, String, Column, Boolean
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(256), nullable=True)
+    surname = Column(String(256), nullable=True)
+    email = Column(String, index=True, nullable=False)
+    is_superuser = Column(Boolean, default=False)
+    recipes = relationship(
+        "Recipe",
+        cascade="all,delete-orphan",
+        back_populates="submitter",
+        uselist=True,
+    )
