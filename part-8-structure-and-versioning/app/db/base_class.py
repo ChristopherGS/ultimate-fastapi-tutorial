5894d9cecff7bf322885395d03bfac707aaa85d2
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2021-07-31 20:03:05 +0100

    add part 8 (#9)
    * add part 8

---
+++ part-8-structure-and-versioning/app/db/base_class.py
@@ -0,0 +1,17 @@
+import typing as t
+
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+class_registry: t.Dict = {}
+
+
+@as_declarative(class_registry=class_registry)
+class Base:
+    id: t.Any
+    __name__: str
+
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
---
+++ part-8-structure-and-versioning/app/db/init_db.py
@@ -0,0 +1,71 @@
+import logging
+from sqlalchemy.orm import Session
+
+from app import crud, schemas
+from app.db import base  # noqa: F401
+from app.core.config import settings
+
+logger = logging.getLogger(__name__)
+
+
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",  # noqa
+    },
+]
+
+
+# make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
+# otherwise, SQL Alchemy might fail to initialize relationships properly
+# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+
+
+def init_db(db: Session) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # Base.metadata.create_all(bind=engine)
+    if settings.FIRST_SUPERUSER:
+        user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
+        if not user:
+            user_in = schemas.UserCreate(
+                full_name="Initial Super User",
+                email=settings.FIRST_SUPERUSER,
+                is_superuser=True,
+            )
+            user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+        else:
+            logger.warning(
+                "Skipping creating superuser. User with email "
+                f"{settings.FIRST_SUPERUSER} already exists. "
+            )
+        if not user.recipes:
+            for recipe in RECIPES:
+                recipe_in = schemas.RecipeCreate(
+                    label=recipe["label"],
+                    source=recipe["source"],
+                    url=recipe["url"],
+                    submitter_id=user.id,
+                )
+                crud.recipe.create(db, obj_in=recipe_in)
+    else:
+        logger.warning(
+            "Skipping creating superuser.  FIRST_SUPERUSER needs to be "
+            "provided as an env variable. "
+            "e.g.  FIRST_SUPERUSER=admin@api.coursemaker.io"
+        )
