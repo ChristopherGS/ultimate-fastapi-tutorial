bb6c01e9b4b7ce78ec0c387658592ad94361417c
ChristopherGS <christopher.samiullah@gmail.com>
2021-07-31 18:06:53 +0100

    simplify crud classes

--- part-7-database/app/db/session.py
+++ part-7-database/app/db/session.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"
 
