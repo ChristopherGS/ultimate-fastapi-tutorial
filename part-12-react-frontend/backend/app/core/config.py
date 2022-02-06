ec9fedd23aa1c405879bfc7f38a2f88e3e5ed079
ChristopherGS <christopher.samiullah@gmail.com>
2022-02-06 19:14:00 +0000

    tidy docker files

--- part-12-react-frontend/backend/app/core/config.py
+++ part-12-react-frontend/backend/app/core/config.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    SQLALCHEMY_DATABASE_URI: Optional[str] = "sqlite:///example.db"
+    SQLALCHEMY_DATABASE_URI: str
     FIRST_SUPERUSER: EmailStr = "admin@recipeapi.com"
     FIRST_SUPERUSER_PW: str = "CHANGEME"
 
--- part-13-docker-deployment/backend/app/run.sh
+++ part-13-docker-deployment/backend/app/run.sh
@@ -18,4 +18,6 @@ export HOST=${HOST:-0.0.0.0}
 export PORT=${PORT:-8001}
 export BACKEND_CORS_ORIGINS=${BACKEND_CORS_ORIGINS}
 
-exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
+
+# run gunicorn
+exec gunicorn --bind $HOST:$PORT "$APP_MODULE" -k uvicorn.workers.UvicornWorker
