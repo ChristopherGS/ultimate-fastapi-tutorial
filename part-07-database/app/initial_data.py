e67590a9ff9e446fbe06f9bf4e432df9c90b2ba2
jncabelin <diethercabelin@gmail.com>
2023-12-28 00:41:12 +0800

    updated references

--- part-07-database/app/initial_data.py
+++ part-07-database/app/initial_data.py
@@ -1,8 +1,8 @@
 import logging
 
-from app.db.base import Base
-from app.db.init_db import init_db
-from app.db.session import SessionLocal
+from db.base import Base
+from db.init_db import init_db
+from db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
