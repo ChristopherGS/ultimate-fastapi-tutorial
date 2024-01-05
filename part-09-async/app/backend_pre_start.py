b5d88c0eb2121e2bba839b40f5def6d24b348ed7
jncabelin <diethercabelin@gmail.com>
2024-01-06 02:31:30 +0800

    reference sqlalchemy text

--- part-09-async/app/backend_pre_start.py
+++ part-09-async/app/backend_pre_start.py
@@ -1,8 +1,8 @@
 import logging
 
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
-
-from app.db.session import SessionLocal
+from sqlalchemy.sql import text
+from db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,7 +21,7 @@ def init() -> None:
     try:
         db = SessionLocal()
         # Try to create session to check if DB is awake
-        db.execute("SELECT 1")
+        db.execute(text("SELECT 1"))
     except Exception as e:
         logger.error(e)
         raise e
