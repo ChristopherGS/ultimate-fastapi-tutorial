3433b416753fb9ec5d49f6cb4aa32e2020f9f682
jncabelin <54582822+jncabelin@users.noreply.github.com>
2024-02-12 04:44:36 +0800

    Part 6b: Support New Python ver <3.12 and Update FastAPI (#52)

--- part-06b-basic-deploy-linode/app/main.py
+++ part-06b-basic-deploy-linode/app/main.py
@@ -51,7 +51,7 @@ def fetch_recipe(*, recipe_id: int) -> Any:
 @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
     *,
-    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
+    keyword: Optional[str] = Query(None, min_length=3, examples="chicken"),
     max_results: Optional[int] = 10,
 ) -> dict:
     """
