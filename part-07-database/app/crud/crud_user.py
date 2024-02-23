9d734be97973571598a2173e79d18412f56d1251
jncabelin <diethercabelin@gmail.com>
2024-02-23 13:50:58 +0800

    updated to SQLAlchemy 2.0 syntax

--- part-07-database/app/crud/crud_user.py
+++ part-07-database/app/crud/crud_user.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, Optional, Union
 
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
@@ -9,7 +10,7 @@ from app.schemas.user import UserCreate, UserUpdate
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        return db.query(User).filter(User.email == email).first()
+        return db.execute(select(User).filter(User.email == email)).scalars().first()
 
     def update(
         self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
